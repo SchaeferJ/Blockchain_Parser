f8e4b2210cc1ce1c12993a1371e9c8da1174da23
SchaeferJ <jocschae@mail.uni-mannheim.de>
2019-08-07 14:57:56 +0200

    Finalized Scraper

--- blockchain_to_csv.py
+++ blockchain_to_csv.py
@@ -1,10 +1,11 @@
+import csv
 import json
-import sys
-import traceback
-import time
-from tqdm import trange
+import os
 import subprocess
+import traceback
 from subprocess import Popen
+
+from tqdm import trange
 
 
 def get_bitcoin_blockcount() -> int:
@@ -21,6 +22,7 @@ def get_bitcoin_blockcount() -> int:
 def get_blockhash(blocknumber: int) -> str:
     """
     Returns the hash of a Bitcoin-Block, given its blocknumber
+
     :param blocknumber:     int, the blocknumber
     :return:                str, the Hash
     """
@@ -66,6 +68,28 @@ def get_transaction(transaction_hash: str, block_height: int) -> dict:
 start_block = 1
 end_block = get_bitcoin_blockcount()
 
+# Initialize CSV files
+BASE_PATH: str = '/hdd/bitcoin-csv'
+
+address_file = open(os.path.join(BASE_PATH, 'addresses.csv'), 'w')
+address_file_w = csv.writer(address_file)
+transaction_file = open(os.path.join(BASE_PATH, 'transactions.csv'), 'w')
+transaction_file_w = csv.writer(transaction_file)
+output_file = open(os.path.join(BASE_PATH, 'outputs.csv'), 'w')
+output_file_w = csv.writer(output_file)
+output_transaction_file = open(os.path.join(BASE_PATH, 'outputs-transactions.csv'), 'w')
+output_transaction_file_w = csv.writer(output_transaction_file)
+output_address_file = open(os.path.join(BASE_PATH, 'outputs-addresses.csv'), 'w')
+output_address_file_w = csv.writer(output_address_file)
+
+address_file_w.writerow(['address'])
+transaction_file_w.writerow(['transaction', 'block'])
+output_file_w.writerow(['output', 'val'])
+output_transaction_file_w.writerow(['output', 'transaction'])
+output_address_file_w.writerow(['output', 'address'])
+
+# Debug
+# counter = 1
 for block_height in trange(start_block, end_block):
     try:
         block_contents = get_block(get_blockhash(block_height))
@@ -73,8 +97,32 @@ for block_height in trange(start_block, end_block):
         print('An Error occurred while trying to parse Blockchain \n' + traceback.format_exc())
 
     for i, tx_hash in enumerate(block_contents['tx']):
-        transaction_data: dict = get_transaction(tx_hash,block_height)
 
-print(get_bitcoin_blockcount())
-print(get_blockhash(1))
-print(type(get_block(get_blockhash(1))))
+        try:
+            transaction_data: dict = get_transaction(tx_hash, block_height)
+        except json.decoder.JSONDecodeError as e:
+            print('An Error occurred while trying to parse Blockchain \n' + traceback.format_exc())
+
+        transaction_file_w.writerow([tx_hash, block_height])
+        transaction_output: dict = transaction_data['vout']
+
+        addresses = list(map(lambda x: x['scriptPubKey']['addresses'], transaction_output))
+        out_values = list(map(lambda x: [x['n'], x['value']], transaction_output))
+        out_transactions = list(map(lambda x: [x['n'], tx_hash], transaction_output))
+        out_addresses = list(map(lambda x: [x['n']] + x['scriptPubKey']['addresses'], transaction_output))
+
+        address_file_w.writerows(addresses)
+        output_file_w.writerows(out_values)
+        output_transaction_file_w.writerows(out_transactions)
+        output_address_file_w.writerows(out_addresses)
+
+    # if counter == 10:
+    #     break
+    # else:
+    #     counter += 1
+
+address_file.close()
+transaction_file.close()
+output_file.close()
+output_transaction_file.close()
+output_address_file.close()
