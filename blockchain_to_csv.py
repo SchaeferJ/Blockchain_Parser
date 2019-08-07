0cfa0298788bea26a506cf528af45c9108d45e53
SchaeferJ <jocschae@mail.uni-mannheim.de>
2019-08-07 12:15:10 +0200

    Setup JSON API Calls

--- blockchain_to_csv.py
+++ blockchain_to_csv.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import traceback
 import time
 from tqdm import trange
 import subprocess
@@ -41,11 +42,38 @@ def get_block(blockhash: str) -> dict:
     return json.loads(output)
 
 
+def decode_transaction(raw_transaction: str, block_height: int) -> dict:
+    cmd: str = 'bitcoin-cli decoderawtransaction {0}'.format(raw_transaction)
+    ps = Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output: Popen = ps.communicate()[0]
+    transaction_json: dict = json.loads(output)
+    return transaction_json
+
+
+def get_transaction(transaction_hash: str, block_height: int) -> dict:
+    """
+
+    :rtype: object
+    """
+    cmd: str = 'bitcoin-cli getrawtransaction {0}'.format(transaction_hash)
+    ps = Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output: Popen = ps.communicate()[0][:-1]
+    output: str = output.decode("UTF-8")
+    transaction_details: dict = decode_transaction(output, block_height)
+    return transaction_details
+
+
 start_block = 1
 end_block = get_bitcoin_blockcount()
 
 for block_height in trange(start_block, end_block):
+    try:
+        block_contents = get_block(get_blockhash(block_height))
+    except json.decoder.JSONDecodeError as e:
+        print('An Error occurred while trying to parse Blockchain \n' + traceback.format_exc())
 
+    for i, tx_hash in enumerate(block_contents['tx']):
+        transaction_data: dict = get_transaction(tx_hash,block_height)
 
 print(get_bitcoin_blockcount())
 print(get_blockhash(1))
