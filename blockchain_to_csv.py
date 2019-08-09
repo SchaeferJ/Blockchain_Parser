7e9650e1cdab13374117c74e9b27412b6d18bec1
SchaeferJ <jocschae@mail.uni-mannheim.de>
2019-08-09 12:33:36 +0200

    Changed output data format

--- blockchain_to_csv.py
+++ blockchain_to_csv.py
@@ -10,6 +10,7 @@ import csv
 import json
 import os
 import traceback
+from datetime import datetime
 
 from tqdm import trange
 
@@ -52,21 +53,51 @@ else:
 # Create output files
 address_file = open(os.path.join(BASE_PATH, 'addresses.csv'), 'w')
 address_file_w = csv.writer(address_file)
+
+blocks_file = open(os.path.join(BASE_PATH, 'blocks.csv'), 'w')
+blocks_file_w = csv.writer(blocks_file)
+
 transaction_file = open(os.path.join(BASE_PATH, 'transactions.csv'), 'w')
 transaction_file_w = csv.writer(transaction_file)
-output_file = open(os.path.join(BASE_PATH, 'outputs.csv'), 'w')
-output_file_w = csv.writer(output_file)
-output_transaction_file = open(os.path.join(BASE_PATH, 'outputs-transactions.csv'), 'w')
-output_transaction_file_w = csv.writer(output_transaction_file)
-output_address_file = open(os.path.join(BASE_PATH, 'outputs-addresses.csv'), 'w')
-output_address_file_w = csv.writer(output_address_file)
+
+before_file = open(os.path.join(BASE_PATH, 'before-rel.csv'), 'w')
+before_file_w = csv.writer(before_file)
+
+belongs_file = open(os.path.join(BASE_PATH, 'belongs-rel.csv'), 'w')
+belongs_file_w = csv.writer(belongs_file)
+
+receives_file = open(os.path.join(BASE_PATH, 'receives-rel.csv'), 'w')
+receives_file_w = csv.writer(receives_file)
+
+sends_file = open(os.path.join(BASE_PATH, 'sends-rel.csv'), 'w')
+sends_file_w = csv.writer(sends_file)
 
 # Initialize column names
-address_file_w.writerow(['address'])
-transaction_file_w.writerow(['transaction', 'block'])
-output_file_w.writerow(['output', 'val'])
-output_transaction_file_w.writerow(['output', 'transaction'])
-output_address_file_w.writerow(['output', 'address'])
+address_file_w.writerow(['address:ID(Address)'])
+address_file_w.writerow(['coinbase'])
+blocks_file_w.writerow(['hash:ID(Block)', 'height:int', 'mediantime:timestamp{timezone:UTC}'])
+transaction_file_w.writerow(['txid:ID(Transaction)'])
+before_file_w.writerow([':START_ID(Block)', ':END_ID(Block)'])
+belongs_file_w.writerow([':START_ID(Transaction)', ':END_ID(Block)'])
+receives_file_w.writerow([':START_ID(Transaction)', 'value', 'output_nr:int', ':END:ID(Address)'])
+sends_file_w.writerow([':START_ID(Address)', 'value', ':END_ID(Transaction)'])
+
+
+def make_sends_list(transaction: dict) -> list:
+    tx_in = transaction['vin']
+    txid = transaction['txid']
+    inputs = []
+    for i, input_tx in enumerate(tx_in):
+        if "coinbase" in input_tx:
+            input_add_tmp = "coinbase"
+            input_value_tmp = transaction['vout'][i]['value']
+        else:
+            input_raw_tx = con.call('getrawtransaction', input_tx['txid'])
+            input_data = con.call('decoderawtransaction', input_raw_tx)
+            input_add_tmp = input_data['vout'][input_tx['vout']]['scriptPubKey']['addresses'][0]
+            input_value_tmp = input_data['vout'][input_tx['vout']]['value']
+        inputs.append([input_add_tmp, input_value_tmp, txid])
+    return inputs
 
 for block_height in trange(START_BLOCK, END_BLOCK):
     try:
@@ -74,8 +105,13 @@ for block_height in trange(START_BLOCK, END_BLOCK):
         block_hash = con.call('getblockhash', block_height)
         # Retrieve Information on selected Block
         block_contents = con.call('getblock', block_hash)
+        # Extract hash of following block
+        next_block_hash = block_contents['nextblockhash']
     except json.decoder.JSONDecodeError as e:
         print('An Error occurred while trying to parse Blockchain \n' + traceback.format_exc())
+    median_time = datetime.utcfromtimestamp(block_contents["mediantime"]).strftime('%Y-%m-%dT%H:%M')
+    blocks_file_w.writerow([block_hash, block_height, median_time])
+    before_file_w.writerow([block_hash, next_block_hash])
 
     # Iterates over all Transactions stored in Block
     for i, tx_hash in enumerate(block_contents['tx']):
@@ -93,23 +129,25 @@ for block_height in trange(START_BLOCK, END_BLOCK):
         # Skip weird transcations that are probably either bugs or attacks on the bitcoin network
         # E.g. Transaction 2a0597e665ac3d1cabeede95cedf907934db7f639e477b3c77b242140d8cf728 in Block #71036
         try:
+            receives = list(
+                map(lambda x: [tx_hash, x['value'], x['n'], x['scriptPubKey']['addresses'][0]], transaction_output))
             addresses = list(map(lambda x: x['scriptPubKey']['addresses'], transaction_output))
-            out_values = list(map(lambda x: [x['n'], x['value']], transaction_output))
-            out_transactions = list(map(lambda x: [x['n'], tx_hash], transaction_output))
-            out_addresses = list(map(lambda x: [x['n']] + x['scriptPubKey']['addresses'], transaction_output))
+            sends = make_sends_list(transaction_data)
         except KeyError:
             print("Irregular transaction encountered in block {0}".format(str(block_height)))
             continue
         # Write CSV files
-        transaction_file_w.writerow([tx_hash, block_height])
+        transaction_file_w.writerow([tx_hash])
+        belongs_file_w.writerow([tx_hash, block_hash])
         address_file_w.writerows(addresses)
-        output_file_w.writerows(out_values)
-        output_transaction_file_w.writerows(out_transactions)
-        output_address_file_w.writerows(out_addresses)
+        receives_file_w.writerows(receives)
+        sends_file_w.writerows(sends)
 
 # Finalize
 address_file.close()
+blocks_file.close()
 transaction_file.close()
-output_file.close()
-output_transaction_file.close()
-output_address_file.close()
+before_file.close()
+belongs_file.close()
+receives_file.close()
+sends_file.close()
